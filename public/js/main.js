53a2fe1cfb393e28906419b6f5b4de57e9ad4d2b
Zakaria <zakPro@live.be>
2021-01-06 22:19:11 +0100

    exo-fini

---
+++ public/js/main.js
@@ -0,0 +1,75 @@
+// EXERCICE 1
+
+// let tabPrenoms = ["zakaria", "luffy", "goku", "zoro", "kurama"];
+
+// let tabLength = tabPrenoms.length
+// console.log(tabPrenoms);
+
+// console.log(`bonjour, ${tabPrenoms[0]}`);
+// console.log(`bonjour, ${tabPrenoms[1]}`);
+// console.log(`bonjour, ${tabPrenoms[2]}`);
+// console.log(`bonjour, ${tabPrenoms[3]}`);
+// console.log(`bonjour, ${tabPrenoms[4]}`);
+
+
+// EXERCICE 2
+
+// let nombreal = Math.floor(Math.random() * 10);
+// let dmd = prompt("induqez un chiffres entre 1 et 10")
+// console.log(nombreal);
+
+// if (dmd == nombreal) {
+//     alert("BIEN JOUER !! tu a gagnée!!");
+// } else {
+//     alert("LOSER !");
+// }
+
+// EXERCICE 3
+
+// let tabQuestion = ["Quelle est la capitale de la Belgique ?", "quel est le nom du fils de goku ?", "quel est mon prenom ?"];
+// let tabReponse = ["Bruxelles", "gohan", "Zakaria"];
+// let tabquestRep = [];
+
+// if (tabQuestion.indexOf[0] == tabReponse.indexOf[0] ) {
+//     alert(`BONNES REPONSE !!`)
+// } if (tabQuestion.indexOf[1] == tabReponse.indexOf[1] ) {
+//     alert(`BONNES REPONSE !!`)
+// } if (tabQuestion.indexOf[2] == tabReponse.indexOf[2] ) {
+//     alert(`BONNES REPONSE !!`)
+// }  else {
+//     alert(`desolé ! mauvaise réponse !`)
+// }
+
+
+// EXERCICE 4 prompt
+
+// let reponse = prompt("quel est le nom du fils de goku ?");
+// console.log(reponse);
+
+// EXERCICE 5 ALERT
+
+// let reponse = prompt("quel est le nom du fils de goku ?");
+// alert(reponse);
+
+// EXERCICE 6 
+
+// let question = prompt(" Quelle temps fait-il aujourd’hui ?");
+
+// let soleil = "soleil"
+// let vent = "vent"
+// let pluie = "pluie"
+// let neige = "neige"
+
+
+// if (question == "soleil") {
+//     console.log("sortez en t-short");
+// } else if (question == "vent") {
+//     console.log("sortez en pull");
+// }  else if (question == "pluie") {
+//     console.log("mettez une veste");
+// }  else if (question == "neige") {
+//     console.log("reste chez toi, il fait froid");
+// }  else {
+//     console.log("j'ai pas compris désolé");
+// }
+
